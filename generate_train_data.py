ef422c0bfeeeb210c44c1ce48e64c0d526ab86d7
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-12 13:49:58 +0200

    Update generate_train_data.py

--- generate_train_data.py
+++ generate_train_data.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+import pandas as pd
+import datetime
+
 from dotenv import load_dotenv
 from src.lib.db import client
-from src.utils.functions import generate_ts_to_index_lookup_table, get_data_from_btc_row
+from src.utils.functions import generate_ts_to_index_lookup_table, get_sma_values_for_row, difference_in_milliseconds
 from src.utils.constants import ROWS_FOR_24h, COLNAME_TO_KEY
-import pandas as pd
+
 
 load_dotenv()
 
@@ -16,6 +19,7 @@ def generate_train_data():
 
     # DB
     db = client(database='crypto')
+    train_data_db = client(database='train')['data']
     collections = db.list_collection_names()
     btc_data = pd.DataFrame(list(db[BTC_COLL_KEY].find({})))
 
@@ -23,24 +27,55 @@ def generate_train_data():
     btc_ts_lookup_table = generate_ts_to_index_lookup_table(
         btc_data)
 
+    inserted_batches = 0
+
     for item in collections:
         if item != BTC_COLL_KEY:
-            data = pd.DataFrame(list(db[item].find({})))
+            collection_data = pd.DataFrame(list(db[item].find({})))
+            batch = []
 
-            for ind in data.index:
+            for ind in collection_data.index:
 
                 if ind - ROWS_FOR_WEEK < 0:
                     continue
 
-                ts = data.iloc[ind][COLNAME_TO_KEY["Kline open time"]]
+                ts = collection_data.iloc[ind][COLNAME_TO_KEY["Kline open time"]]
                 btc_row_index = btc_ts_lookup_table[ts]
 
                 train_data_row = {}
 
-                get_data_from_btc_row(
-                    data=btc_data, train_data_row=train_data_row, btc_row_index=btc_row_index)
+                start = datetime.datetime.now()
 
-                print(train_data_row)
+                get_sma_values_for_row(
+                    data=collection_data,
+                    train_data_row=train_data_row,
+                    curr_row_index=ind,
+                    key_pre_fix='coin')
+
+                end = datetime.datetime.now()
+
+                print('get_sma_values_for_row: ' +
+                      difference_in_milliseconds(start, end))
+
+                get_sma_values_for_row(
+                    data=btc_data,
+                    train_data_row=train_data_row,
+                    curr_row_index=btc_row_index,
+                    key_pre_fix='btc')
+
+                batch.append(train_data_row)
+
+                if len(batch) == 100:
+
+                    batched_data_df = pd.concat(batch, axis=0)
+                    batched_data_df.reset_index(inplace=True)
+                    batched_data_df.drop(['index'], inplace=True, axis=1)
+                    batched_data_dict = batched_data_df.to_dict('records')
+                    train_data_db.insert_many(batched_data_dict)
+
+                    inserted_batches += 1
+                    print(inserted_batches)
+                    batch = []
 
 
 generate_train_data()
