31a58f870c1ade77b511f0bfd8899d322469fe43
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-12 10:54:45 +0200

    Change /data dir to project root

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 import os
-from process_raw_data.raw_data_to_db import write_csv_to_db
-from lib.db import client
+from src.process_raw_data.raw_data_to_db import write_csv_to_db
+from src.lib.db import client
 from dotenv import load_dotenv
 
 
@@ -11,7 +13,7 @@ def main():
 
     db = client(database=os.environ.get('MONGO_DB'))
 
-    write_csv_to_db(db['DOGEUSDT-5m-klines'], path='process_raw_data/data')
+    write_csv_to_db(db['DOGEUSDT-5m-klines'], path='data')
 
 
 main()
