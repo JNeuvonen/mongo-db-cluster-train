d00558db4d9799e39b626fe3c13536ef9e8ec52c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-23 11:43:40 +0200

    Rewrite generate_train_data func

--- generate_train_data.py
+++ generate_train_data.py
@@ -4,7 +4,7 @@ import datetime
 
 from dotenv import load_dotenv
 from src.lib.db import client
-from src.utils.functions import generate_ts_to_index_lookup_table, get_sma_values_for_row, difference_in_milliseconds
+from src.utils.functions import add_sma_from_binance_kline
 from src.utils.constants import ROWS_FOR_24h, COLNAME_TO_KEY
 
 
@@ -15,7 +15,6 @@ def generate_train_data():
 
     # UTIL
     BTC_COLL_KEY = "BTCUSDT-5m-klines"
-    ROWS_FOR_WEEK = ROWS_FOR_24h * 7
 
     # DB
     db = client(database='crypto')
@@ -23,59 +22,28 @@ def generate_train_data():
     collections = db.list_collection_names()
     btc_data = pd.DataFrame(list(db[BTC_COLL_KEY].find({})))
 
-    # TS TO INDEX LOOKUP TABLE
-    btc_ts_lookup_table = generate_ts_to_index_lookup_table(
-        btc_data)
-
-    inserted_batches = 0
-
     for item in collections:
         if item != BTC_COLL_KEY:
             collection_data = pd.DataFrame(list(db[item].find({})))
-            batch = []
 
-            for ind in collection_data.index:
+            final_df = pd.DataFrame()
 
-                if ind - ROWS_FOR_WEEK < 0:
-                    continue
+            merged_df = pd.merge(
+                collection_data, btc_data, on='k')
 
-                ts = collection_data.iloc[ind][COLNAME_TO_KEY["Kline open time"]]
-                btc_row_index = btc_ts_lookup_table[ts]
+            merged_df.drop(['_id_x', '_id_y', 'i_y'], axis=1, inplace=True)
 
-                train_data_row = {}
+            add_sma_from_binance_kline(
+                final_df, merged_df, ROWS_FOR_24h * 7)
 
-                start = datetime.datetime.now()
+            add_sma_from_binance_kline(
+                final_df, merged_df, ROWS_FOR_24h)
 
-                get_sma_values_for_row(
-                    data=collection_data,
-                    train_data_row=train_data_row,
-                    curr_row_index=ind,
-                    key_pre_fix='coin')
+            add_sma_from_binance_kline(
+                final_df, merged_df, int(ROWS_FOR_24h / 24))
 
-                end = datetime.datetime.now()
-
-                print('get_sma_values_for_row: ' +
-                      difference_in_milliseconds(start, end))
-
-                get_sma_values_for_row(
-                    data=btc_data,
-                    train_data_row=train_data_row,
-                    curr_row_index=btc_row_index,
-                    key_pre_fix='btc')
-
-                batch.append(train_data_row)
-
-                if len(batch) == 100:
-
-                    batched_data_df = pd.concat(batch, axis=0)
-                    batched_data_df.reset_index(inplace=True)
-                    batched_data_df.drop(['index'], inplace=True, axis=1)
-                    batched_data_dict = batched_data_df.to_dict('records')
-                    train_data_db.insert_many(batched_data_dict)
-
-                    inserted_batches += 1
-                    print(inserted_batches)
-                    batch = []
+            df_dict = final_df.to_dict('records')
+            train_data_db.insert_many(df_dict)
 
 
 generate_train_data()
