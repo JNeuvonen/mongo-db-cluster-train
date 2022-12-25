daa0d8875fa6dfed48b56df66fb3130454a88150
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-25 12:21:04 +0200

    Add backtest sim to test model quality

--- test_model.py
+++ test_model.py
@@ -14,7 +14,7 @@ model = torch.load('model.pt')
 
 # DATA
 coll = client(database='train')['data']
-DOCS_LIMIT = 100000
+DOCS_LIMIT = 10000
 
 
 data = []
@@ -23,9 +23,9 @@ for doc in coll.find({}).limit(DOCS_LIMIT):
     df = pd.DataFrame(doc, index=range(len(doc.keys())))
     data.append(df)
 
-
 # FORMAT DATA
 test_data = pd.concat(data)
+target_data = test_data['target']
 test_data.drop(['target', '_id', 'target'], axis=1, inplace=True)
 test_data = test_data.to_numpy()
 test_data = torch.from_numpy(test_data)
@@ -35,8 +35,26 @@ test_data = torch.nn.functional.normalize(test_data)
 test_data = torch.where(torch.isnan(
     test_data), torch.zeros_like(test_data), test_data)
 
-pred_tensor = model(test_data)
-pred_df = pd.DataFrame(pred_tensor.detach().numpy())
+balance = 10000
+index = 0
+ticks_invested = 0
+short_bets = 0
+long_bets = 0
+chart_of_balance = [balance]
 
-pred_df.hist()
+for item in test_data:
+    pred = model(item)
+    pred = pred.detach().numpy()[0] + 1
+    target = target_data.iloc[index] + 1
+
+    if pred > 1.05 and ticks_invested >= 30:
+        balance = balance * target
+        ticks_invested = 0
+        long_bets += 1
+        chart_of_balance.append(balance)
+
+    index += 1
+    ticks_invested += 1
+
+plt.plot(chart_of_balance)
 plt.show()
